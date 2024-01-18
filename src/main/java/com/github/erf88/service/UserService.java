0cb310255842c0884c68569abbb2ec6f9125a2fc
ERF88 <erf_3d@hotmail.com>
2024-01-18 09:11:54 -0300

    add impl to delete user contract and handle not found

--- src/main/java/com/github/erf88/service/UserService.java
+++ src/main/java/com/github/erf88/service/UserService.java
@@ -22,9 +22,7 @@ public class UserService {
     }
 
     public Mono<User> findById(String id) {
-        return repository.findById(id)
-                .switchIfEmpty(Mono.error(new ObjectNotFoundException("Object not found. Id: %s, Type: %s"
-                        .formatted(id, User.class.getSimpleName()))));
+        return handleNotFound(repository.findById(id), id);
     }
 
     public Flux<User> findAll() {
@@ -35,6 +33,15 @@ public class UserService {
         return findById(id)
                 .map(entity -> mapper.ToEntity(request, entity))
                 .flatMap(repository::save);
+    }
+
+    public Mono<User> delete(final String id) {
+        return handleNotFound(repository.findAndRemove(id), id);
+    }
+
+    private <T> Mono<T> handleNotFound(Mono<T> mono, String id) {
+        return mono.switchIfEmpty(Mono.error(new ObjectNotFoundException("Object not found. Id: %s, Type: %s"
+                .formatted(id, User.class.getSimpleName()))));
     }
 
 }
