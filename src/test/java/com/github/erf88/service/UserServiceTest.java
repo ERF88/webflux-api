ba9ef1f2480d437f68b3bb6f1729a1c4dafc2720
ERF88 <erf_3d@hotmail.com>
2024-01-18 09:25:41 -0300

    add unit test to save user service

---
+++ src/test/java/com/github/erf88/service/UserServiceTest.java
@@ -0,0 +1,51 @@
+package com.github.erf88.service;
+
+import com.github.erf88.entity.User;
+import com.github.erf88.mapper.UserMapper;
+import com.github.erf88.model.request.UserRequest;
+import com.github.erf88.repository.UserRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+import java.util.Objects;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class UserServiceTest {
+
+    @Mock
+    private UserRepository repository;
+
+    @Mock
+    private UserMapper mapper;
+
+    @InjectMocks
+    private UserService userService;
+
+    @Test
+    void save() {
+        UserRequest request = new UserRequest("usuario", "usuario@email.com", "usuario123");
+        User entity = User.builder().build();
+
+        when(mapper.toEntity(any(UserRequest.class))).thenReturn(entity);
+        when(repository.save(any(User.class))).thenReturn(Mono.just(User.builder().build()));
+
+        Mono<User> result = userService.save(request);
+
+        StepVerifier.create(result)
+                .expectNextMatches(Objects::nonNull)
+                .expectComplete()
+                .verify();
+
+        verify(repository, times(1)).save(any(User.class));
+    }
+}
