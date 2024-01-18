39fcf50489de582efd6d51416dd1a591d2180ab8
ERF88 <erf_3d@hotmail.com>
2024-01-18 09:33:13 -0300

    add unit test to findAll user service

--- src/test/java/com/github/erf88/service/UserServiceTest.java
+++ src/test/java/com/github/erf88/service/UserServiceTest.java
@@ -10,6 +10,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
@@ -61,6 +62,20 @@ class UserServiceTest {
                 .verify();
 
         verify(repository, times(1)).findById(anyString());
+    }
+
+    @Test
+    void testFindAll() {
+        when(repository.findAll()).thenReturn(Flux.just(User.builder().build()));
+
+        Flux<User> result = userService.findAll();
+
+        StepVerifier.create(result)
+                .expectNextMatches(user -> user.getClass() == User.class)
+                .expectComplete()
+                .verify();
+
+        verify(repository, times(1)).findAll();
     }
 
 }
